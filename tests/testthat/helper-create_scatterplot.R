6ade4afc1e8d7ae42b58e32e0de0a891fa72435a
Edwin Yeung <edsters78@gmail.com>
2024-04-10 22:04:21 -0700

    removed script dependencies in some helper files

--- tests/testthat/helper-create_scatterplot.R
+++ tests/testthat/helper-create_scatterplot.R
@@ -1,7 +1,7 @@
 # function inputs for tests for create_scatterplot
 
 library(ggplot2)
-source("../../R/create_scatterplot.R")
+
 
 body_data <- data.frame("height" = c(1.61, 1.50, 1.78, 1.86, 1.52),
                         "weight" = c(45, 40, 67, 81, 51))
